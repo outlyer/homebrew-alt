9576f27243eef215597fe810a8832e8e4ba6cf64
Aubin Paul <git@outlyer.org>
2014-08-25 09:28:09 -0400

    Don't know why this wasn't loading from upstream

---
+++ Casks/nzbdrop.rb
@@ -0,0 +1,7 @@
+class Nzbdrop < Cask
+  url 'https://www.asar.com/download/NZBDrop.dmg'
+  homepage 'https://www.asar.com/nzbdrop.html'
+  version '3.0.1'
+  sha256 '579536d54b36a0b4cfdb367fea3c3b47e44d45ee3ba187b15244bc1e0368fec4'
+  link 'NZB Drop.app'
+end
