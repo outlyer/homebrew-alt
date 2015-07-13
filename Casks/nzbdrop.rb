20eb42a2344f84059d581586131a5ec03a0b9cb9
Aubin Paul <git@outlyer.org>
2015-07-13 11:26:09 -0400

    Update some personal formulas

--- Casks/nzbdrop.rb
+++ Casks/nzbdrop.rb
@@ -1,7 +1,9 @@
-class Nzbdrop < Cask
+cask :v1 => 'nzbdrop' do
   url 'https://www.asar.com/download/NZBDrop.dmg'
   homepage 'https://www.asar.com/nzbdrop.html'
   version '3.0.1'
   sha256 '579536d54b36a0b4cfdb367fea3c3b47e44d45ee3ba187b15244bc1e0368fec4'
-  link 'NZB Drop.app'
+  app 'NZB Drop.app'
+  license :closed
+  name "NZBDrop"
 end
--- rmlint.rb
+++ rmlint.rb
@@ -2,12 +2,15 @@ require 'formula'
 
 class Rmlint < Formula
   homepage 'https://github.com/sahib/rmlint'
-  url 'https://github.com/sahib/rmlint/archive/1.0.6b.tar.gz'
-  version '1.0.6b'
-  sha1 'c72dd04627a0814c473a35c88eaccfdacdd0d1d1'
+  url 'https://github.com/sahib/rmlint/archive/v1.0.6.tar.gz'
+  version '1.0.6'
+  sha1 'b1d87bb148b235481733a8353dd3a98866ecee1f'
 
   head 'https://github.com/sahib/rmlint.git', :branch => 'master'
 
+  #depends_on "scons" => :build
+  #depends_on "glib" => :build
+ 
   def install
     inreplace "Makefile", "/usr", ""
     system "make", "install", "DESTDIR=#{prefix}"
