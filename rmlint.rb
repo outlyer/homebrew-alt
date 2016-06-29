8d55c0d7760205343ad211f4d735b8ced010b274
Aubin Paul <git@outlyer.org>
2016-06-29 10:06:17 -0400

    Upgrade rmlint to 2.4.4 since it works on Mac OS X now

--- rmlint.rb
+++ rmlint.rb
@@ -2,9 +2,10 @@ require 'formula'
 
 class Rmlint < Formula
   homepage 'https://github.com/sahib/rmlint'
-  url 'https://github.com/sahib/rmlint/archive/v1.0.6.tar.gz'
-  version '1.0.6'
-  sha1 'b1d87bb148b235481733a8353dd3a98866ecee1f'
+  url 'https://github.com/sahib/rmlint/archive/v2.4.4.tar.gz'
+  version '2.4.4'
+  sha256 '294708e7c98783a7782df1ed7f6fc79e9036571b7f69f76c5b3455545ce568bc'
+  depends_on "scons" => :build
 
   head 'https://github.com/sahib/rmlint.git', :branch => 'master'
 
@@ -12,7 +13,6 @@ class Rmlint < Formula
   #depends_on "glib" => :build
  
   def install
-    inreplace "Makefile", "/usr", ""
-    system "make", "install", "DESTDIR=#{prefix}"
+    system "scons", "install", "--prefix=#{prefix}"
   end
 end
