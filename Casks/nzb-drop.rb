a8a86a7ba9fb93979d2224494cb33cc2a59fbfc7
Aubin Paul <git@outlyer.org>
2016-10-19 08:28:18 -0400

    Long overdue update to keep up to date with upstream changes
    * Remove formulas which have moved into homebrew core
    * Update nzbdrop cask to match new Caskroom conventions

---
+++ Casks/nzb-drop.rb
@@ -0,0 +1,9 @@
+cask 'nzb-drop' do
+  version :latest
+  sha256 :no_check
+  url 'http://www.asar.com/download/NZBDrop.dmg'
+  name "NZBDrop"
+  homepage 'https://www.asar.com/nzbdrop.html'
+  app 'NZB Drop.app'
+  license :closed
+end
