3514e79318b391227a85f27102cce69dfd2893d4
Aubin Paul <outlyer@users.noreply.github.com>
2018-05-23 23:00:11 -0400

    Remove old cask

--- Casks/nzb-drop.rb
+++
@@ -1,9 +0,0 @@
-cask 'nzb-drop' do
-  version :latest
-  sha256 :no_check
-  url 'http://www.asar.com/download/NZBDrop.dmg'
-  name "NZBDrop"
-  homepage 'https://www.asar.com/nzbdrop.html'
-  app 'NZB Drop.app'
-  license :closed
-end
