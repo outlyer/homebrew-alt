4c14aa57508aa159156814455799889de9eb609b
Aubin Paul <outlyer@users.noreply.github.com>
2019-02-19 15:32:35 -0500

    Try generic build

--- archi-steam-farm-bin.rb
+++ archi-steam-farm-bin.rb
@@ -1,7 +1,7 @@
 class ArchiSteamFarmBin < Formula
   desc "A C# (mono) application that allows you to farm steam cards using multiple steam accounts simultaneously."
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
-  url "https://github.com/JustArchiNET/ArchiSteamFarm/releases/download/4.0.0.8/ASF-osx-x64.zip"
+  url "https://github.com/JustArchiNET/ArchiSteamFarm/releases/download/4.0.0.8/ASF-generic.zip"
   sha256 "3b6967ee94ade610f51e695a20868163684d2c77f97f7d81afd9657fa11b0e4a"
   version "4.0.0.8"
 
