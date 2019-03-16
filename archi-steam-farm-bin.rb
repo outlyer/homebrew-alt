7f7aaf928beebc9a65fce98faa0235e12a41cb2d
Aubin Paul <outlyer@users.noreply.github.com>
2019-03-16 12:07:35 -0400

    Fixed

--- archi-steam-farm-bin.rb
+++ archi-steam-farm-bin.rb
@@ -2,7 +2,7 @@ class ArchiSteamFarmBin < Formula
   desc "A C# (mono) application that allows you to farm steam cards using multiple steam accounts simultaneously."
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
   url "https://github.com/JustArchiNET/ArchiSteamFarm/releases/download/4.0.0.8/ASF-generic.zip"
-  sha256 "3b6967ee94ade610f51e695a20868163684d2c77f97f7d81afd9657fa11b0e4a"
+  sha256 "7b1bbe6a7e19df688f86c1915a769003e3b2251667f6ef6cf536e822f00528ff"
   version "4.0.0.8"
 
   bottle :unneeded
