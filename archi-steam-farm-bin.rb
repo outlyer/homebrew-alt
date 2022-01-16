4744638a300489be29eeae601e781da76f367501
Aubin Paul <outlyer@users.noreply.github.com>
2022-01-16 13:07:22 -0500

    Update Archi-Steam-Farm to 5.2.1.5

--- archi-steam-farm-bin.rb
+++ archi-steam-farm-bin.rb
@@ -1,9 +1,9 @@
 class ArchiSteamFarmBin < Formula
   desc "A C# (mono) application that allows you to farm steam cards using multiple steam accounts simultaneously."
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
-  version "5.2.0.10"
+  version "5.2.1.5"
   url "https://github.com/JustArchiNET/ArchiSteamFarm/releases/download/#{version}/ASF-osx-x64.zip"
-  sha256 "3076fa7771b03bcbc161b0a456a06052bee6e08a54c73952fbdafb08beba25fd"
+  sha256 "11b6045a0353ecc4d1ed03a2088e2e710905dda0fe030be744933a1282a1be5b"
 
   def install
     libexec.install Dir["*"]
