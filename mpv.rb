de52460e278b789e45d2952d4efe9e920bea3e2f
Aubin Paul <outlyer@users.noreply.github.com>
2023-07-23 15:44:47 -0400

    Update mpv to 0.36.0

--- mpv.rb
+++ mpv.rb
@@ -1,73 +1,71 @@
 class Mpv < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.35.1.tar.gz"
-  sha256 "41df981b7b84e33a2ef4478aaf81d6f4f5c8b9cd2c0d337ac142fc20b387d1a9"
+  url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.36.0.tar.gz"
+  sha256 "29abc44f8ebee013bb2f9fe14d80b30db19b534c679056e4851ceadf5a5e8bf6"
   license :cannot_represent
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
-  depends_on "pkg-config" => :build
-  depends_on "python@3.11" => :build
+  depends_on "meson" => :build
+  depends_on "pkg-config" => [:build, :test]
   depends_on xcode: :build
-
   depends_on "outlyer/alt/ffmpeg"
-# depends_on "outlyer/alt/libplacebo"
+  depends_on "jpeg-turbo"
+  depends_on "libarchive"
   depends_on "libass"
-# depends_on "jpeg"
-# depends_on "libarchive"
-# depends_on "little-cms2"
-# depends_on "mujs"
+  depends_on "luajit"
+  depends_on "mujs"
   depends_on "uchardet"
   depends_on "yt-dlp"
-  depends_on "luajit-openresty"
-  depends_on "jpeg-turbo"
-  depends_on "vapoursynth" => :optional
-  depends_on "jack" => :optional
-  depends_on "libaacs" => :optional
-  depends_on "libbluray" => :optional
-  depends_on "libcaca" => :optional
-  depends_on "libdvdnav" => :optional
-  depends_on "libdvdread" => :optional
-  depends_on "pulseaudio" => :optional
-  depends_on "rubberband" => :optional
 
   def install
-    # LANG is unset by default on macOS and causes issues when calling getlocale
+     # LANG is unset by default on macOS and causes issues when calling getlocale
     # or getdefaultlocale in docutils. Force the default c/posix locale since
     # that's good enough for building the manpage.
     ENV["LC_ALL"] = "C"
 
+    # force meson find ninja from homebrew
+    ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
+
     # libarchive is keg-only
-    #ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
-    # luajit-openresty is keg-only
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["luajit-openresty"].opt_lib/"pkgconfig"
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
 
     args = %W[
-      --prefix=#{prefix}
-      --confdir=#{etc}/mpv
+      -Dhtml-build=disabled
+      -Djavascript=enabled
+      -Dlibmpv=true
+      -Dlua=luajit
+      -Dlibarchive=enabled
+      -Duchardet=enabled
+      --sysconfdir=#{pkgetc}
       --datadir=#{pkgshare}
       --mandir=#{man}
-      --docdir=#{doc}
-      --zshdir=#{zsh_completion}
-      --enable-lua
-      --lua=luajit
-      --enable-uchardet
     ]
 
-    args << "--enable-libbluray" if build.with? "libbluray"
-    args << "--enable-dvdnav" if build.with? "libdvdnav"
-    args << "--enable-dvdread" if build.with? "libdvdread"
-    args << "--enable-pulse" if build.with? "pulseaudio"
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
 
-    python3 = "python3.11"
-    system python3, "bootstrap.py"
-    system python3, "waf", "configure", *args
-    system python3, "waf", "install"
-    system python3, "TOOLS/osxbundle.py", "build/mpv"
-    prefix.install "build/mpv.app"
+    if OS.mac?
+      # `pkg-config --libs mpv` includes libarchive, but that package is
+      # keg-only so it needs to look for the pkgconfig file in libarchive's opt
+      # path.
+      libarchive = Formula["libarchive"].opt_prefix
+      inreplace lib/"pkgconfig/mpv.pc" do |s|
+        s.gsub!(/^Requires\.private:(.*)\blibarchive\b(.*?)(,.*)?$/,
+                "Requires.private:\\1#{libarchive}/lib/pkgconfig/libarchive.pc\\3")
+      end
+  end
+
+    bash_completion.install "etc/mpv.bash-completion" => "mpv"
+    zsh_completion.install "etc/_mpv.zsh" => "_mpv"
   end
 
   test do
-    system bin/"mpv", "--ao=null", test_fixtures("test.wav")
+    system bin/"mpv", "--ao=null", "--vo=null", test_fixtures("test.wav")
+    #assert_match "vapoursynth", shell_output(bin/"mpv --vf=help")
+
+    # Make sure `pkg-config` can parse `mpv.pc` after the `inreplace`.
+    system "pkg-config", "mpv"
   end
 end
