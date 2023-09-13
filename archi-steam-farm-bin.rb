aad763ce7e0fa8d12dad92ec460a810e45226869
Aubin Paul <outlyer@users.noreply.github.com>
2023-09-13 19:53:30 -0400

    Update to 5.4.9.3

--- archi-steam-farm-bin.rb
+++ archi-steam-farm-bin.rb
@@ -1,14 +1,14 @@
 class ArchiSteamFarmBin < Formula
   desc "A C# (mono) application that allows you to farm steam cards using multiple steam accounts simultaneously."
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
-  version "5.4.3.2"
+  version "5.4.9.3"
 
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    sha256 "aa7d651886599cba9451ccde518dd56e59753c9181189f16895cabb176558473"
+    sha256 "a4d3a097b4eeebaf6c5b089c89c2a7c6804540007fde46e3cef731da3ca20e40"
   else
-    sha256 "d63d4f046982f8a13b73d59b4b2ac600a0c27db0e447972111babd09d118f715"
+    sha256 "cfb96dbc7504a763cccdf0233a5073aa46409b6a2b423b4768b712ff906f29c7"
   end
   url "https://github.com/JustArchiNET/ArchiSteamFarm/releases/download/#{version}/ASF-osx-#{arch}.zip"
 
