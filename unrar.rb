b66c5cbe15ae718475120b09e7ccb4bc92be5079
Aubin Paul <outlyer@users.noreply.github.com>
2024-03-13 11:26:50 -0400

    Update unrar to 7.0.7

--- unrar.rb
+++ unrar.rb
@@ -1,8 +1,8 @@
 class Unrar < Formula
   desc "Extract, view, and test RAR archives"
   homepage "https://www.rarlab.com/"
-  url "https://www.rarlab.com/rar/unrarsrc-7.0.3.tar.gz"
-  sha256 "2df43254f8132937d409934673755e76152357045468af87331c7b5fe48cced4"
+  url "https://www.rarlab.com/rar/unrarsrc-7.0.7.tar.gz"
+  sha256 "da95829c7e66fe461c06eb4bea8145e58d88d76909432d0875cd1ff86669f728"
 
   livecheck do
     url "https://www.rarlab.com/rar_add.htm"
@@ -14,7 +14,7 @@ class Unrar < Formula
     # so we do the dirty work of renaming their shared objects to
     # dylibs for them.
     inreplace "makefile", "libunrar.so", "libunrar.dylib"
-    inreplace "makefile", "CXXFLAGS=-march=native -O2 -Wno-logical-op-parentheses -Wno-switch -Wno-dangling-else","CXXFLAGS=-march=native -O2 -Wno-logical-op-parentheses -Wno-switch -Wno-dangling-else -std=c++11"
+    #inreplace "makefile", "CXXFLAGS=-march=native -O2 -Wno-logical-op-parentheses -Wno-switch -Wno-dangling-else","CXXFLAGS=-march=native -O2 -Wno-logical-op-parentheses -Wno-switch -Wno-dangling-else -std=c++11"
     system "make"
     bin.install "unrar"
 
