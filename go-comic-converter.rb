c95051198ee25ccc678f6b70be938b6e18b86fbe
Aubin Paul <outlyer@users.noreply.github.com>
2024-05-30 13:31:29 -0400

    Update to latest go-comic-converter

--- go-comic-converter.rb
+++ go-comic-converter.rb
@@ -1,15 +1,18 @@
 class GoComicConverter < Formula
   desc "Convert CBZ/CBR/Dir into EPUB for e-reader devices (Kindle Devices, Apple Books, etc.)"
   homepage "https://github.com/celogeek/go-comic-converter"
-  url "https://github.com/celogeek/go-comic-converter/archive/refs/tags/v2.6.9.tar.gz"
-  sha256 "24fb457b355b8ce0645ddf02f660bd5f583b719a955d380c6c907de9941a5cf3"
+  version "2.7.2"
+  url "https://github.com/celogeek/go-comic-converter/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "2f0483741dd316e368f3169c5694adedd2b79b60476d716ce8f9d346b81030a0"
   license "MIT"
-  head "hhttps://github.com/celogeek/go-comic-converter.git", branch: "main"
+  head "https://github.com/celogeek/go-comic-converter.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X main.version=#{version}"
+    ldflags = %W[
+      -s -w
+    ]
     system "go", "build", *std_go_args(ldflags:), "main.go"
   end
 
